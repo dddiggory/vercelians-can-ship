db23441761d02044c822c8109b69ce7c32b22e33
Diggory Rycroft <diggory.rycroft@vercel.com>
2023-10-13 19:34:50 -0400

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,14 +1,55 @@
-//you can overwrite this entire file with your v0 Component.
-//just copy and paste over all of it.
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/9YE2zTtqZKB
+ */
+import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
 
-import React from 'react';
-
-function TestComponent() {
-    return (
-    <div className="hidden">
-        <h1>You can&apos;t see me, but soon I&apos;m going to be a v0 component.</h1>
-    </div>
-    )
+export default function Component() {
+  return (
+    <Card key="1" className="mx-auto max-w-md bg-green-200 border-dashed border-green-500">
+      <CardHeader>
+        <CardTitle className="text-xl text-green-800 animate-bounce duration-2000">Guestbook</CardTitle>
+        <CardDescription className="text-green-600">Leave a message for us before you go</CardDescription>
+      </CardHeader>
+      <CardContent className="bg-green-100">
+        <div className="space-y-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="space-y-2">
+              <Label className="text-green-600" htmlFor="first-name">
+                Name
+              </Label>
+              <Input className="bg-green-50 text-green-700" id="first-name" placeholder="Your Name" required />
+            </div>
+            <div className="space-y-2">
+              <Label className="text-green-600" htmlFor="email">
+                Email
+              </Label>
+              <Input className="bg-green-50 text-green-700" id="email" placeholder="Your Email" required type="email" />
+            </div>
+          </div>
+          <div className="space-y-2">
+            <Label className="text-green-600" htmlFor="message">
+              Message
+            </Label>
+            <Textarea
+              className="min-h-[100px] bg-green-50 text-green-700"
+              id="message"
+              placeholder="Leave a nice message"
+              required
+            />
+          </div>
+          <Button
+            className="w-full bg-green-500 hover:bg-green-600 text-white animate-pulse border-2 border-green-700"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
-
-export default TestComponent;
