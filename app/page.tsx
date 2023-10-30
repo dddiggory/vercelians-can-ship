6271494d65793161f336dc75533fbb088795758b
Diggory Rycroft <diggory.rycroft@vercel.com>
2023-10-30 17:17:16 +0000

    minor style fixes for empty state

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default async function Home(url: any, city: any, region: any, country: an
       <div>
         {!profileData ? (
           <div>
-            <p className="fixed left-0 top-0 w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <p className="fixed left-0 top-0 w-full p-8 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           
             Welcome!  This is a starting point for your personal Vercelian info page.
             Start by visiting <a href="https://github.com/dddiggory/vercelians-can-ship#welcome-vercelian" target="_blank" className="underline text-blue-800">&apos;the Template repo on Github&apos;</a>. Follow the instructions in the Readme to make your own copy and deploy it to Vercel.
@@ -220,7 +220,7 @@ export default async function Home(url: any, city: any, region: any, country: an
           />
         </div>
 
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <div className="pt-32 lg:pt-0 mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
