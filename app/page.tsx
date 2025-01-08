5db103e24d9ea7d981042d9868c5c986f9681976
Joe DeSapio <104697630+joethehuman@users.noreply.github.com>
2025-01-08 09:59:32 -0800

    Update page.tsx to fix build failures.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 // For an initial starting point, insert your GitHub username between the double quotes here.
 // Letter casing doesn't matter.
+import { EmptyState } from "./components/empty-state";
+import { NotFound } from "./components/not-found";
+import { Profile } from "./components/profile";
+import { getGithubProfile } from "./lib/get-github-profile";
+
 const yourGithubUsername = "";
 
-// During the workshop, leave the code below as-is.
-// But when you're ready, don't be afraid to jump in, make edits, try new things, and make it your own!
-import {EmptyState} from "./components/empty-state";
-import {NotFound} from "./components/not-found";
-import {Profile} from "./components/profile";
-import {getGithubProfile} from "./lib/get-github-profile";
-
-const githubToken = process.env.GITHUB_TOKEN ? process.env.GITHUB_TOKEN : false;
-
-export default async function Home(url: any) {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const profileData = await getGithubProfile(yourGithubUsername);
-  const geo = url.searchParams;
 
   if (!profileData) {
     return <EmptyState />;
@@ -23,5 +22,5 @@ export default async function Home(url: any) {
     return <NotFound username={yourGithubUsername} />;
   }
 
-  return <Profile username={yourGithubUsername} geo={geo} />;
+  return <Profile username={yourGithubUsername} geo={searchParams} />;
 }
